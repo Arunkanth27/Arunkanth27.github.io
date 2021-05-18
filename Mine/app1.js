45504c8329e4185a3e17247bde47cbfd6260fe5d
Arunkanth27 <ak524607@gmail.com>
2021-05-18 13:52:55 +0530

    js

--- Mine/app1.js
+++ Mine/app1.js
@@ -1,2 +1,19 @@
-var msg = 'Hello World';
-console.log(msg);
+
+
+// var age =[175, 50, 25];
+
+
+// function func(total,age)
+// {
+//   return total+age;
+// }
+// console.log(age.reduce(func));
+
+var points = [40, 100, 1, 5, 25, 10];
+
+console.log( n =points.includes(1000));
+console.log(points.sort(function(a,b) {
+  return a-b;
+} ));
+
+
